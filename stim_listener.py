402a753f6a8bf5c35fc65c16a9da05879a36a293
Obley, JP <jpobley@umich.edu>
2013-07-13 11:45:54 -0700

    Use urlparse for getting queries. Clean up comments, remove unused imports

--- stim_listener.py
+++ stim_listener.py
@@ -2,9 +2,8 @@
 # Local server for fNIRS trials that records stim marks and timings
 ######
 
-import os, sys, logging, time
-#import bottle
-#from bottle import  response, request, run, route
+import os, logging, time, serial
+from urlparse import urlparse, parse_qs
 
 import BaseHTTPServer
 
@@ -13,35 +12,50 @@ import BaseHTTPServer
 ####
 class StimRequestHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     
-    # handle a GET
     def do_GET(self):
-           # want to extract the state=[1248] part of the path 
-           print self.path
            
-           #get request params
            try:
-               # have to parse the req params from the path string yourself - so put them into a dictionary (not super robust)
-               param_str=self.path.split('?')[1]
-               params = dict([(param.split('=')) for param in param_str.split('&')])
-               print params
-               state = params['state']
+               result = urlparse(self.path)
+               params = parse_qs(result.query)
+               state = params['state'][0]
                aux = state_codes[state]
-               msg = params['msg']
+               msg = params['msg'][0]
                if state == "start":
-                   pID = params['pID']
+                   pID = params['pID'][0]
                    setup_log(pID)
                log_msg = "%f\t%s" % (time.time(), msg)
                logging.debug(log_msg)
-               
-               
+               self.send_response(200)
+
            except:
                print 'error'
+               self.send_response(500)
+           
+           self.end_headers()
+           return
+           
+
+####
+# run the HTTP server
+#####
+
+def run_server():
+    """Main function that runs the server"""
+    server_address = ('127.0.0.1', 1234)
+    httpd = BaseHTTPServer.HTTPServer(server_address, StimRequestHTTPHandler)
+    print('http server is running...')
+    httpd.serve_forever()
 
 ######
 # dict for mapping widget event codes to auxiliary serials ports
 ######
 
 state_codes = {"recording":2, "stopped recording":4, "start": 8, "stop": 8, "new_stim": 1}
+
+
+######
+# function to create unique log file for this session
+######
 
 def setup_log(pID):
     """Check for logs folder, create it if it doesn't exist. Create unique log file for each session"""
@@ -62,19 +76,8 @@ def setup_log(pID):
                         format='%(message)s',
                         level=logging.DEBUG)
 
-####
-# run the HTTP server
-#####
-
-def run_server():
-    server_address = ('127.0.0.1', 1234)
-    httpd = BaseHTTPServer.HTTPServer(server_address, StimRequestHTTPHandler)
-    print('http server is running...')
-    httpd.serve_forever()
-    
 ######
 # run application
 ######
 
 run_server()
-
