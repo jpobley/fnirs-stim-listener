c515366177a8fd9cb6b82440acdc288a16f2cf0d
Obley, JP <jpobley@umich.edu>
2013-07-13 11:15:51 -0700

    Setup basic idea for the listener

---
+++ stim_listener.py
@@ -0,0 +1,23 @@
+######
+# Local server for fNIRS trials that records stim marks and timings
+######
+
+import os, sys, logging, time
+import bottle
+from bottle import  response, request, run, route
+
+######
+# create serial port object and mapping dictionary for upload widget codes
+######
+
+######
+# function to create unique log file for this session
+######
+
+######
+# define route
+######
+
+######
+# run application
+######
