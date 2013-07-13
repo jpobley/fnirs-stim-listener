3f9818ffa115aa0092e2dd82af430d3b7647b8a4
Obley, JP <jpobley@umich.edu>
2013-07-13 11:39:20 -0700

    Changes by mbod to use listener in PsychoPy by copying BaseHTTPServer into directory. It works for some reason

--- stim_listener.py
+++ stim_listener.py
@@ -3,12 +3,39 @@
 ######
 
 import os, sys, logging, time
-import bottle
-from bottle import  response, request, run, route
+#import bottle
+#from bottle import  response, request, run, route
 
-######
-# create serial port object.mapping dictionary for upload widget codes
-######
+import BaseHTTPServer
+
+#####
+# define custom HTTP handler
+####
+class StimRequestHTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    
+    # handle a GET
+    def do_GET(self):
+           # want to extract the state=[1248] part of the path 
+           print self.path
+           
+           #get request params
+           try:
+               # have to parse the req params from the path string yourself - so put them into a dictionary (not super robust)
+               param_str=self.path.split('?')[1]
+               params = dict([(param.split('=')) for param in param_str.split('&')])
+               print params
+               state = params['state']
+               aux = state_codes[state]
+               msg = params['msg']
+               if state == "start":
+                   pID = params['pID']
+                   setup_log(pID)
+               log_msg = "%f\t%s" % (time.time(), msg)
+               logging.debug(log_msg)
+               
+               
+           except:
+               print 'error'
 
 ######
 # dict for mapping widget event codes to auxiliary serials ports
@@ -35,25 +62,19 @@ def setup_log(pID):
                         format='%(message)s',
                         level=logging.DEBUG)
 
+####
+# run the HTTP server
+#####
 
-######
-# define route
-######
-
-@route('/stim')
-def start():
-    state = request.query.state
-    aux = state_codes[state]
-    msg = request.query.msg
-    if state == "start":
-        pID = request.query.pID
-        setup_log(pID)
-    log_msg = "%f\t%s" % (time.time(), msg)
-    logging.debug(log_msg)
+def run_server():
+    server_address = ('127.0.0.1', 1234)
+    httpd = BaseHTTPServer.HTTPServer(server_address, StimRequestHTTPHandler)
+    print('http server is running...')
+    httpd.serve_forever()
     
 ######
 # run application
 ######
 
-run(host='127.0.0.1', port=1234)
+run_server()
 
