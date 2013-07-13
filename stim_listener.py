adc9b01a4733cb85b4cb77b28b5162a97a9e3320
Obley, JP <jpobley@umich.edu>
2013-07-13 11:20:41 -0700

    Create function for logging each session

--- stim_listener.py
+++ stim_listener.py
@@ -7,12 +7,34 @@ import bottle
 from bottle import  response, request, run, route
 
 ######
-# create serial port object and mapping dictionary for upload widget codes
+# create serial port object.mapping dictionary for upload widget codes
 ######
 
 ######
-# function to create unique log file for this session
+# dict for mapping widget event codes to auxiliary serials ports
 ######
+
+state_codes = {"recording":2, "stopped recording":4, "start": 8, "stop": 8, "new_stim": 1}
+
+def setup_log(pID):
+    """Check for logs folder, create it if it doesn't exist. Create unique log file for each session"""
+    cd = os.getcwd()
+    logdir = '%s/logs' % (cd)
+    if not os.path.exists(logdir):
+        os.mkdir(logdir)
+    logname = '%s/%s' % (logdir, pID)
+    logfile = "%s.txt" % (logname)
+    count = 1
+    while os.path.exists(logfile):
+        temp_name = "%s_%d" % (logname, count)
+        logfile = "%s.txt" % (temp_name)
+        count += 1
+
+    logging.basicConfig(filename=logfile, 
+                        filemode="w",
+                        format='%(message)s',
+                        level=logging.DEBUG)
+
 
 ######
 # define route
